0ca65dd1b0c2eff17d5fe350f9537422f4d3ca5e
CorinneEarl <corinneaearl@gmail.comgit commit --amend --reset-authorgit statusgit config --global user.email corinneaearl@gmail.com>
2017-03-08 14:30:48 -0700

    updated login and signup

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,18 @@
+import react;
 
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+    			<input type="text" class="username" placeholder="Enter username">
+    			<input type="password" class="password" placeholder="Enter password">
+    			<input type="button" class="login" value="Login">
+    		</form>
+    		<div class="underForm">Don't have an account? Sign up <a href="./signup.html">here!</a></div>
+      </div>
+    )
+  }
+}
 
-
-
-   <div id="root"></div>
-    <header>loose-leaf</header>
-    <form>
-			<input type="text" class="username" placeholder="Enter username">
-			<input type="password" class="password" placeholder="Enter password">
-			<input type="button" class="login" value="Login">
-		</form>
-		<div class="underForm">Don't have an account? Sign up <a href="./signup.html">here!</a></div>
+export default Login;
--- src/Signup.js
+++ src/Signup.js
@@ -2,22 +2,20 @@ import react;
 
 class Signup extends Component {
   render() {
-    return ( 
-
- <div id="root"></div>
-    <header>loose-leaf</header>
-    <form>
-			<input type="text" class="textInput" id="firstName" placeholder="First name">
-			<input type="text" class="textInput" id="lastName" placeholder="Last name">
-			<input type="text" class="textInput" id="username" placeholder="Username">
-			<input type="password" class="textInput" id="password" placeholder="Password">
-			<input type="password" class="textInput" id="confirmPassword" placeholder="Confirm password">
-			<input type="email" class="textInput" id="emailInput" placeholder="Email address">
-      <input type="button" class="textInput" id="createAccount" value="Create account">
-		</form>
-		<div class="underForm">Already have an account? <a href="./login.html">Login</a></div>
-
-	);
+    return (
+      <div>
+        <form>
+    			<input type="text" class="textInput" id="firstName" placeholder="First name">
+    			<input type="text" class="textInput" id="lastName" placeholder="Last name">
+    			<input type="text" class="textInput" id="username" placeholder="Username">
+    			<input type="password" class="textInput" id="password" placeholder="Password">
+    			<input type="password" class="textInput" id="confirmPassword" placeholder="Confirm password">
+    			<input type="email" class="textInput" id="emailInput" placeholder="Email address">
+          <input type="button" class="textInput" id="createAccount" value="Create account">
+    		</form>
+    		<div class="underForm">Already have an account? <a href="./login.html">Login</a></div>
+      </div>
+	   );
   }
 }
 
