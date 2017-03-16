511ae1df2fedb007f0b9e5e29561b02e89a169dd
g-crow <genevieve.crow@gmail.com>
2017-03-16 16:45:46 -0600

    thursday afternoon experiments in resizablegit status

--- src/components/MyFirstGrid.js
+++ src/components/MyFirstGrid.js
@@ -1,21 +1,50 @@
 import React, {Component} from 'react'
-import {Responsive, WidthProvider} from 'react-grid-layout';
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+import ReactGridLayout from 'react-grid-layout'
+import { Resizable, ResizableBox } from 'react-resizable';
+// import {Responsive, WidthProvider} from 'react-grid-layout';
+// const ResponsiveReactGridLayout = WidthProvider(Responsive);
 // var ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
 
-class MyFirstGrid extends Component {
-  render() {
-    var layouts;
-    return (
-      <ResponsiveReactGridLayout className="layout" layouts={layouts}
-        breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-        cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}>
-        <div key={"1"}>
-        </div>
-        <div key={"2"}>2</div>
-        <div key={"3"}>3</div>
-      </ResponsiveReactGridLayout>
-    )
+export default class MyFirstGrid extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: 200,
+      height: 200,
+      message: ""
+    };
   }
-};
-export default MyFirstGrid;
+  
+  onResize = (event, {element, size}) => {
+    this.setState({width: size.width, height: size.height});
+  };
+
+  onClick = () => {
+    this.setState({width: 100, height: 100, message: "you're cool"});
+  };
+
+
+  render() {
+  return (
+
+    <div>
+
+    <div>
+        <button onClick={this.onClick} style={{'marginBottom': '10px'}}>Reset first element's width/height</button>
+    </div>
+
+    <div>
+    <Resizable className="box" className="tester1" height={this.state.height} width={this.state.width} onResize={this.onResize}>
+            <div className="box" style={{width: this.state.width + 'px', height: this.state.height + 'px'}}>
+              <span className="text">{this.state.message}</span>
+            </div>
+          </Resizable>
+
+      </div>
+
+      </div>
+  )
+}
+}
