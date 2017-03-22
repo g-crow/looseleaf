42299f41035ab43fbe41a7014ea9727b4e9b9806
g-crow <genevieve.crow@gmail.com>
2017-03-22 14:30:31 -0600

    fixed deployment

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var dev = process.env.DEV
 module.exports = {
   'secret': 'ihatejavascript',
   'database': process.env.MONGODB_URI || 'mongodb://localhost/Looseleaf',
-  'webpack': !DEV ? '/' : 'http://localhost:3000',
-  'serverRoute':!DEV ? '/api' : 'http://localhost:3002/api',
-  'deployed' : !DEV,
+  'webpack': !dev ? '/' : 'http://localhost:3000',
+  'serverRoute':!dev ? '/api' : 'http://localhost:3002/api',
+  'deployed' : !dev,
   port : process.env.PORT || 3002
   // 'usernamePlaceholder': "User's Name"
 }
