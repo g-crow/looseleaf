e3d287c90718a4506f82c75ce6dcdd30d62e1988
g-crow <genevieve.crow@gmail.com>
2017-03-23 15:03:32 -0600

    fixed dev flag

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-var dev = true
+var dev = process.env.NODE_ENV === 'development'
 module.exports = {
   'secret': 'ihatejavascript',
   'database': process.env.MONGODB_URI || 'mongodb://localhost/Looseleaf',
