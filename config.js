c39dea56ffddd09f3d6507bd1e544f8841f3b2d8
g-crow <genevieve.crow@gmail.com>
2017-03-25 15:31:13 -0600

    config

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
+
+var dev = process.env.NODE_ENV === 'development'
+module.exports = {
+  'secret': 'ihatejavascript',
+  'database': process.env.MONGODB_URI || 'mongodb://localhost/Looseleaf',
+  'webpack': !dev ? '/' : 'http://localhost:3000',
+  'serverRoute':!dev ? '/api' : 'http://localhost:3002/api',
+  'deployed' : !dev,
+  port : process.env.PORT || 3002
+}
+
+
 // module.exports = {
 //   'secret': 'ihatejavascript',
 //   'database': 'mongodb://localhost/Looseleaf',
@@ -9,12 +21,3 @@
 //
 //
 // Config for heroku deployment:
-var dev = process.env.NODE_ENV === 'development'
-module.exports = {
-  'secret': 'ihatejavascript',
-  'database': process.env.MONGODB_URI || 'mongodb://localhost/Looseleaf',
-  'webpack': !dev ? '/' : 'http://localhost:3000',
-  'serverRoute':!dev ? '/api' : 'http://localhost:3002/api',
-  'deployed' : !dev,
-  port : process.env.PORT || 3002
-}
